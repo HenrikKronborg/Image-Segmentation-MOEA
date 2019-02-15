c0250f364d54d649a797bc8d4e03c756a585c663
Henrik Kronborg <henrkron@stud.ntnu.no>
2019-02-15 11:27:32 +0100

    Load image

--- controller/Main.java
+++ controller/Main.java
@@ -1,12 +1,23 @@
 package controller;
 
-import java.awt.image.BufferedImage;
-
+import model.Position;
 import model.functions.ImageLoader;
+
+import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String... args) {
+        ImageLoader image = new ImageLoader();
+        image.loadImage("86016.jpg");
 
-        BufferedImage image = new BufferedImage();
+        Color[][] pixels = image.getPixels();
+
+        for(Color[] i : pixels) {
+            for(Color j : i) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
     }
 }
