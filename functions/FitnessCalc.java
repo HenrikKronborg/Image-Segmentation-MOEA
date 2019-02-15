8db7f938f5672bf69eb4b6497cb22dfb5793ce03
Henrik <henrkron@stud.ntnu.no>
2019-02-15 09:50:57 +0100

    Functions package

--- functions/FitnessCalc.java
+++ functions/FitnessCalc.java
@@ -1,4 +1,4 @@
-package controller;
+package functions;
 
 public class FitnessCalc {
     public FitnessCalc() {
--- functions/ImageLoader.java
+++ functions/ImageLoader.java
@@ -1,4 +1,4 @@
-package controller;
+package functions;
 
 import model.Position;
 
