96c4b7016fbb8271e6487251897131b83154232e
stianStensli <stian.hs@hotmail.com>
2019-02-18 14:56:36 +0100

    validator

--- model/functions/Validators.java
+++ model/functions/Validators.java
@@ -1,11 +1,50 @@
 package model.functions;
 
+import model.Solution;
+
+import java.util.List;
+
 public class Validators {
 
-    public static boolean validateRank(){
+    public static boolean validateRank(List<List<Solution>> rankedPopulation){
+        // Step 1: is there any solution in a rank that dominates another?
+        for(List<Solution> rank : rankedPopulation){
+            for(Solution ind : rank){
+                for(Solution cmp : rank){
+                    if(!ind.equals(cmp)){
+                        if(cmp.getFitnessConnectivity() <= ind.getFitnessConnectivity()){
+                            if(cmp.getFitnessDeviation() <= ind.getFitnessDeviation()){
+                                if(cmp.getFitnessDeviation() != ind.getFitnessDeviation() || cmp.getFitnessConnectivity() != ind.getFitnessConnectivity())
+                                    return false;
+                            }
+                        }
+                    }
+                }
+            }
 
+        }
+        // Step 2: a solution has to be dominated by a solution in the previous rank.
+        // Step 3: a solution can not dominate a solution in the previous rank.
+        for(int i = 1; i < rankedPopulation.size(); i++){
+            for(Solution ind : rankedPopulation.get(i)){
+                boolean dominated = false;
 
+                for(Solution cmp : rankedPopulation.get(i-1)){
+                    if(cmp.getFitnessConnectivity() > ind.getFitnessConnectivity() && cmp.getFitnessDeviation() > ind.getFitnessDeviation()){
+                        dominated = true;
 
-        return false;
+                    }
+                    if(ind.getFitnessConnectivity() > cmp.getFitnessConnectivity() && ind.getFitnessDeviation() > cmp.getFitnessDeviation())
+                        return false;
+                }
+
+                if(!dominated)
+                    return false;
+
+            }
+
+        }
+
+        return true;
     }
 }
