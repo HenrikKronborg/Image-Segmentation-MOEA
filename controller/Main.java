87cbb07b8dd3e7f3a5e8d064d6b1bef89336f878
HenrikKronborg <henrkron@stud.ntnu.no>
2019-03-07 11:39:03 +0100

    Clean

--- controller/Main.java
+++ controller/Main.java
@@ -10,20 +10,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
-
-        /*
-        System.out.println("\nTest Chrom:");
-        //int[] gene = new int[]{1,5,1,3,0,4,2,3,9,10,14,7,8,12,15,15};
-
-        Chromosome c = new Chromosome();
-        c.generateRandomGene();
-        ArrayList<Segment> segments = c.generatePhenotype();
-        System.out.println("test:" + segments.size());
-
-        FitnessCalc f = new FitnessCalc();
-        f.setImageLoader(image);
-        System.out.println(Arrays.toString(f.generateFitness(segments)));
-        */
     }
 
     public void start(Stage primaryStage) throws Exception {
