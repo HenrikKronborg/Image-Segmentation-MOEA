fa1e33624ae54df911897a32a0d3820ba81a09bd
stianStensli <stian.hs@hotmail.com>
2019-03-16 17:59:19 +0100

    popsize 40

--- controller/GeneticAlgorithm.java
+++ controller/GeneticAlgorithm.java
@@ -3,7 +3,7 @@ package controller;
 import model.supportNodes.ThreadNode;
 
 public interface GeneticAlgorithm {
-    int popSize = 16; // Population size
+    int popSize = 40; // Population size
     int numOffsprings = popSize; // Number of offsprings
     double mutationRate = 0.20; // Mutation rate
     int maxRuns = 20; // Maximum number of runs before termination
